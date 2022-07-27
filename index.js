aff07507d0e5567aba6150ca477e9ff3470066f3
Williard Baez <willy09154@gmail.com>
2022-07-26 17:50:53 -0700

    Offset y pagination

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const urlParams = new URLSearchParams(location.search);
+
 var http = new XMLHttpRequest();
-var url = 'https://pokeapi.co/api/v2/pokemon';
+var limit = urlParams.has('limit') ? urlParams.get('limit') : 20;
+var offset = urlParams.has('offset') ? urlParams.get('offset') : 0;
+var url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
 http.onreadystatechange = function() {
     var response = JSON.parse(http.responseText);
     var tbody = document.querySelector('#pokemon-table tbody');
+    var tfoot = document.querySelector('#pokemon-table tfoot');
 
     response.results.forEach((element, index) => {
         tbody.innerHTML += `<tr> \
@@ -12,6 +17,9 @@ http.onreadystatechange = function() {
             <td><a href="pokemon.html?id=${index + 1}">View more</a></td> \
         </tr>`;
     });
+
+    const nextParams = new URLSearchParams(response.next);
+    tfoot.innerHTML = `<a href="index.html?offset=${nextParams.get('offset')}&limit=${nextParams.get('limit')}" class="btn btn-primary">Next</a>`;
 }
 
 http.open('GET', url, false);
